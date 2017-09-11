6e52f99d14f4b9bb02fe78f3ffa140a8205ea449
Eric Zell <ezell@rbmtechnologies.com>
2017-09-11 18:53:25 -0400

    CRUD for Admin Users

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -5,20 +5,50 @@ class AdminUsersController < ApplicationController
   before_action :confirm_logged_in
 
   def index
-    @users = AdminUser.sorted
+    @admin_users = AdminUser.sorted
   end
 
   def new
-    @user = AdminUser.new
+    @admin_user = AdminUser.new
   end
 
   def create
-
+    @admin_user = AdminUser.new(admin_user_params)
+    if @admin_user.save
+      flash[:notice] = "New Admin User '#{@admin_user.username}' created successfully"
+      redirect_to(action: :index)
+    else
+      render(:new)
+    end
   end
 
   def edit
+    @admin_user = AdminUser.find(params[:id])
+  end
+
+  def update
+    @admin_user = AdminUser.find(params[:id])
+    if @admin_user.update_attributes(admin_user_params)
+      flash[:notice] = "Admin User '#{@admin_user.username}' updated successfully"
+      redirect_to(action: :index)
+    else
+      render(:edit)
+    end
   end
 
   def delete
+    @admin_user = AdminUser.find(params[:id])
+  end
+
+  def destroy
+    admin_user = AdminUser.find(params[:id]).destroy
+    flash[:notice] = "Admin User '#{admin_user.name}' destroyed successfully."
+    redirect_to(action: :index)
+  end
+
+  private
+
+  def admin_user_params
+    params.require(:admin_user).permit(:first_name, :last_name, :email, :email_confirmation, :username, :password)
   end
 end
